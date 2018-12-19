336b78bf48dd04f516c289659286100ebd3690f7
luis manuel fernandez <lmfintonic.com>
2018-12-19 22:44:56 +0100

    styles: fonts

--- src/components/Fonts/index.js
+++ src/components/Fonts/index.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
+function variantColor({ theme, variant = 'black' }) {
+  return theme.colors[`${variant}`];
+}
+
 export const H1 = styled.h1`
   font-family: 'Quicksand', sans-serif;
   line-height: 60px;
   font-size: 70px;
   margin-top: 0;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+  font-weight: 300;
 `;
 
 export const H2 = styled.h2`
@@ -15,6 +21,8 @@ export const H2 = styled.h2`
   line-height: 36px;
   margin-top: 0;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+  font-weight: 300;
 
   @media (max-width: 768px) {
     font-size: 18px;
@@ -28,6 +36,8 @@ export const H3 = styled.h3`
   font-size: 35px;
   margin-top: 0;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+  font-weight: 300;
 
   @media (max-width: 768px) {
     font-size: 16px;
@@ -40,24 +50,42 @@ export const H4 = styled.h4`
   font-size: 28px;
   margin-top: 0;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+  font-weight: 400;
 `;
 
 export const H5 = styled.h5`
   font-family: 'Quicksand', sans-serif;
   line-height: 32px;
-  font-size: 20px;
+  font-size: 18px;
   margin-top: 0;
   text-transform: uppercase;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+  font-weight: bold;
 `;
 
 export const Body = styled.p`
   font-family: 'Quicksand', sans-serif;
-  line-height: 32px;
   font-size: 18px;
   line-height: 25px;
   margin-top: 0;
   text-rendering: optimizeLegibility;
+  color: ${variantColor};
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    line-height: 23px;
+  }
+`;
+
+export const Caps = styled.p`
+  font-family: 'Quicksand', sans-serif;
+  font-size: 15px;
+  line-height: 22px;
+  margin-top: 0;
+  text-rendering: optimizeLegibility;
+  color: ${variantColor};
 
   @media (max-width: 768px) {
     font-size: 16px;
