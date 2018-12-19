dec67a64449852753d8beca3095ea23493fa9291
luis manuel fernandez <lmfintonic.com>
2018-12-19 22:44:29 +0100

    feat: add global styles

--- src/styles/styled-global.js
+++ src/styles/styled-global.js
@@ -6,26 +6,18 @@ const StyledGlobal = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: ${({ theme }) => theme.baseFont};
-  }
-
   body {
     margin: 0pt;
-    color: ${({ theme }) => theme.colors.grayColor};
+    color: ${({ theme }) => theme.colors.black};
     font-family: CerebriSans-Light, sans-serif;
-    background-color: #fbfafa;
-
-    @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
-      font-size: ${rem('18px')};
-    }
+    background-color: ${({ theme }) => theme.colors.gray};
   }
 
   main {
     margin: 0 auto;
     width: 100%;
     max-width: 1500px;
-    background-color: #fff;
+    background-color: ${({ theme }) => theme.colors.white};
     left: 0;
     z-index: 1;
   }
@@ -38,10 +30,6 @@ const StyledGlobal = createGlobalStyle`
     color: ${({ theme }) => theme.colors.blueColor};
   }
 
-  // Delete the yellow color of inputs
-  input:-webkit-autofill {
-    -webkit-box-shadow: 0 0 0px 1000px ${({ theme }) => theme.colors.whiteColor} inset;
-  }
 `;
 
 export default StyledGlobal;
